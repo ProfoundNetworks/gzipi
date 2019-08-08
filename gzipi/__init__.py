aa4071ee0f7af540768e06b750555a33d3e958bd
Artem Golubin <me@rushter.com>
2019-08-08 20:24:50 +0300

    Expose search function

--- gzipi/__init__.py
+++ gzipi/__init__.py
@@ -4,7 +4,7 @@
 #
 """Top-level package for gzipi."""
 
-from .lib import index_csv_file, index_json_file, retrieve
+from .lib import index_csv_file, index_json_file, retrieve, search
 from .lib import repack_json_file, repack_csv_file
 
 __version__ = '0.1.0'
