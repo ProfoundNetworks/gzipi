7fc7273f88297e20e567ac9fd6a1038beb3c989c
Michael Penkov <m@penkov.dev>
2019-08-20 16:22:04 +0900

    fix description, improve version handling

--- gzipi/__init__.py
+++ gzipi/__init__.py
@@ -3,8 +3,11 @@
 # (C) Copyright: Profound Networks, LLC 2019
 #
 """Top-level package for gzipi."""
+import os.path as P
 
 from .lib import index_csv_file, index_json_file, retrieve, search
 from .lib import repack_json_file, repack_csv_file
 
-__version__ = '0.1.0'
+
+with open(P.join(P.dirname(__file__), 'VERSION')) as fin:
+    __version__ = fin.readline().rstrip()
--- setup.py
+++ setup.py
@@ -3,15 +3,25 @@
 
 """The setup script."""
 
+import io
+import os.path as P
 from setuptools import setup, find_packages
 
 
+def read(fname):
+    return io.open(P.join(P.dirname(__file__), fname), encoding='utf-8').read()
+
+
+version = read('gzipi/VERSION').split('\n')[0]
 requirements = ['smart-open', 'plumbum']
 setup_requirements = []
 test_requirements = ['pytest', 'flake8', ]
 
 setup(
     author="Profound Networks",
+    author_email='mpenkov@profound.net',
+    url='https://github.com/ProfoundNetworks/gzipi',
+    download_url='https://pypi.org/project/gzipi',
     classifiers=[
         'Intended Audience :: Developers',
         'Natural Language :: English',
@@ -24,13 +34,19 @@ setup(
     ],
     install_requires=requirements,
     include_package_data=True,
+    package_data={
+        'gzipi': [
+            'VERSION',
+        ],
+    },
     name='gzipi',
     packages=find_packages(include=['gzipi']),
     setup_requires=setup_requirements,
     test_suite='tests',
-    desription="Tools for indexing gzip files to support random-like access.",
+    description="Tools for indexing gzip files to support random-like access.",
+    long_description=read('README.rst'),
     tests_require=test_requirements,
-    version='0.1.0',
+    version=version,
     zip_safe=False,
     entry_points={
         'console_scripts': [
